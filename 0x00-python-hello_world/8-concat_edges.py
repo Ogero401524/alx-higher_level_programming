cb5634c398285e821b5b795ae72ec44c3bd41f73
Ogero401524 <amirakraphael@gmail.com>
2023-11-30 01:19:03 -0800

    codes are run lllllllbylllllllllllllllll

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(' ')[-6] + str.split(' ')[-5] + str.split(' ')[-4] +
-      str.split(' ')[-3] + str.split(' ')[-2])
+str = (str[39:66] + " " + str[-22:-18] + " " + str[:6])
 print(str)
