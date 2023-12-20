800ace2f795c5b8360ecd10cec6bb0b12a949d6d
Ogero401524 <amirakraphael@gmail.com>
2023-12-20 02:34:11 -0800

    deburged return frm loop to set at end

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end= " ")
-           return PRINTEL =+ 1
+            PRINTEL += 1
     except IndexError:
         pass
     print()
+    return PRINTEL
