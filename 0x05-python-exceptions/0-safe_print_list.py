38a78c6479f735b55ea3d0cf0cb5f6edf46e07b3
Ogero401524 <amirakraphael@gmail.com>
2023-12-20 02:24:02 -0800

    loop with defence mecanisims

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    PRINTEL = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end= " ")
+           return PRINTEL =+ 1
+    except IndexError:
+        pass
+    print()
