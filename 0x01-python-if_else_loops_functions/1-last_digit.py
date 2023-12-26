bd00161dee8fb735393a45c7f07b1e6755bc20ae
Ogero401524 <amirakraphael@gmail.com>
2023-12-25 16:55:53 -0800

    module to get last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+# the last digit of the value of the number
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+
+# print the result based on the last digit
+if lastdigit > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, lastdigit))
+elif lastdigit < 6 and lastdigit != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, lastdigit))
+else:
+    print("Last digit of {:d} is 0 and is 0".format(number))
