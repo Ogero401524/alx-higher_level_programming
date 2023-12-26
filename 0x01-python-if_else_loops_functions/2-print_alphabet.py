106639e66d38ed1a458ea9d3c37aed4aea59729c
Ogero401524 <amirakraphael@gmail.com>
2023-12-25 18:54:08 -0800

    insomnia alphabet gmae

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-#asci values
+# asci values
 start = ord('a')
 end = ord('z')
 
-#loop through and prrint
+# loop through and prrint
 for asciiValue in range(start, end + 1):
     print('{:c}'.format(asciiValue), end='')
