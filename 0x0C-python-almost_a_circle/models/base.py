6a9edfd615208a556b809331999ca3536cae584b
Ogero401524 <amirakraphael@gmail.com>
2024-01-19 06:01:26 -0800

    base csae instances of a class and their manuplation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
+"""Defines a base model class."""
 class Base:
-    """Class  constructor for Base."""
+    """Base model.
+
+    This Represents the "base" for all other classes in project 0x0C*.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Class constructor for Base.
+
+        Parameters:
+        id (int, optional): Unique identifier. If provided, assign it to
+        the instance's `id` attribute.
+        If None, increment the class attribute `__nb_objects` and
+        assign its value to `id`.
+        """
         if id is not None:
             self.id = id
         else:
