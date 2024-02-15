37eb2f7469fa0fe84fd5116e4472e3fff65eefd1
Ogero401524 <amirakraphael@gmail.com>
2024-02-15 13:30:41 -0800

    CREATE TB with values

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- a script that creates a table second_table with multi values
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256))
+VALUES((1, "John", 10),(2, "Alex", 3),(3, "Bob", 14),(4, "George", 8));
