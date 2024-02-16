f8e7c8abd592c62f32f1e49bd475d690904d99ec
Ogero401524 <amirakraphael@gmail.com>
2024-02-15 16:21:54 -0800

    temp sql func

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- a script that displays the average temperature (Fahrenheit) by city ordered
-SELECT city, AVG(value) AS avg_tempp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY avg_tempp DESC;
+ORDER BY avg_temp DESC;
