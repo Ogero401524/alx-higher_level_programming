9e39e6a8ca8afce5ca7a972ca559d0c27c9fcc4b
Ogero401524 <amirakraphael@gmail.com>
2024-03-06 14:40:25 -0800

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,50 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *turtle = list;	// 'turtle' moves one node at a time
-	listint_t *hare = list;		// 'hare' moves two nodes at a time
+    listint_t *turtle = list;  // 'turtle' moves one node at a time
+    listint_t *hare = list;    // 'hare' moves two nodes at a time
 
-	if (!list)
-		return (0);
+    while (turtle && hare && hare->next)
+    {
+        turtle = turtle->next;
+        hare = hare->next->next;
 
-	while (turtle && hare && hare->next)
-	{
-		turtle = turtle->next;
-		hare = hare->next->next;
-		if (turtle == hare)
-			return (1);	//yes
-	}
+        if (turtle == hare)
+            return (1);  // There is a cycle
+    }
 
-	return (0);	// No
+    return (0);  // No cycle found
+}
+
+
+int main(void)
+{
+
+    listint_t *list = malloc(sizeof(listint_t));
+    head->n = 1;
+
+    list->next = malloc(sizeof(listint_t));
+    list->next->n = 2;
+
+    list->next->next = malloc(sizeof(listint_t));
+    list->next->next->n = 3;
+
+    // Create a cycle
+    list->next->next->next = list;
+
+    // Test the function
+    int result = check_cycle(list);
+
+    // Output the result
+    if (result)
+        printf("The linked list has a cycle.\n");
+    else
+        printf("The linked list does not have a cycle.\n");
+
+    // Clean up memory
+    free(list->next->next);
+    free(list->next);
+    free(list);
+
+    return 0;
 }
