977f39b8524d3f76a49cbff1420cbcd2727176d8
Ogero401524 <amirakraphael@gmail.com>
2024-03-24 11:32:42 -0700

    list tanle in db with balue use format method to grab

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    dbase = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], dbase=sys.argv[3], port=3306)
+    cursa = dbase.cursor()
+    cursa.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cursa.fetchall()
+    for row in rows:
+        print(row)
+    cursa.close()
+    dbase.close()
