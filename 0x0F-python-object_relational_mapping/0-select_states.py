b4bc5057f1275a56ff4df7fdb9aabf17bebe4c30
Ogero401524 <amirakraphael@gmail.com>
2024-03-24 11:01:48 -0700

    list tanle in db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""list all states """
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    listdb = MySQLdb.connect(host="localhost",
+                             userr=sys.argv[1],pwsd=sys.argv[2],
+                             listdb=sys.argv[3], port=3306)
+    pointer = listdb.cursor()
+    pointer.execute("SELECT * FROM states")
+    r = pointer.fetchall()
+    for rw in r:
+        print(rw)
+    pointer.close()
+    listdb.close()
