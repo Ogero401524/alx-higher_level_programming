9fc4237f3f019fab3e0fd49804f447a138c455e6
Ogero401524 <amirakraphael@gmail.com>
2024-09-18 05:22:38 -0700

    concat join return

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.count += 1
-    return ", ".join(["BestSchool"] * magic_string.count)
-
-magic_string.count = 0
+def magic_string(n=[0]):
+    n[0] += 1
+    return ", ".join(["BestSchool"] * n[0])
